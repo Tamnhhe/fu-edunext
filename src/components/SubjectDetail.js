173ef5f100315156a1300b7fb693a52d081a3a86
Tamnhhe <tamnhhe@gmail.com>
2024-07-11 01:55:28 +0700

    Update SubjectDetail.js

--- src/components/SubjectDetail.js
+++ src/components/SubjectDetail.js
@@ -1,28 +1,31 @@
-import React, { useContext } from 'react'
-import { useParams } from 'react-router-dom'
-import { UserContext } from '../context/UserContext'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { CardFooter } from "react-bootstrap";
 function SubjectDetail() {
-    const {slots,questions} = useContext(UserContext);
-    const { id } = useParams();
-    const filterSlot = slots.filter(slot => slot.subjectid == id);
+  const { slots, questions } = useContext(UserContext);
+  const { id } = useParams();
+  const filterSlot = slots.filter((slot) => slot.subjectid == id);
+  const filterQuestion = questions.filter((question) => question.slotid == id);
   return (
     <div>
-        {filterSlot.map((slot,index) => (
-            <Card>
-            <Card.Header>Slot {index+1}</Card.Header>
-            <Card.Body>
-              <Card.Title>{slot.title}</Card.Title>
-              <Card.Text>
-                With supporting text below as a natural lead-in to additional content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        ))}
+      {filterSlot.map((slot, index) => (
+        <Card>
+          <Card.Header>Slot {index + 1}</Card.Header>
+          <Card.Body>
+            <Card.Title>{slot.title}</Card.Title>
+          </Card.Body>
+          <CardFooter>
+            {filterQuestion.map((question, index) => (
+              <Card.Text>{question.title}</Card.Text>
+            ))}
+          </CardFooter>
+        </Card>
+      ))}
     </div>
-  )
+  );
 }
 
-export default SubjectDetail
+export default SubjectDetail;
