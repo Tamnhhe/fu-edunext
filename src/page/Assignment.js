31a8fb7b1742b6da2b40d20e0f5e649d6051074e
HungNDHE171455 <hungndhe171455@fpt.edu.vn>
2024-07-11 15:19:28 +0700

    cập nhật assig
    cậ nhật asign

--- src/page/Assignment.js
+++ src/page/Assignment.js
@@ -1,11 +1,13 @@
 import React,{ useContext } from 'react';
 import { UserContext } from '../context/UserContext'; // Import UserContext
-import { Navigate } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
 import { Card, Container, Row, Col } from 'react-bootstrap';
-
+import ClassIcon from "@mui/icons-material/Class";
+import SubjectIcon from "@mui/icons-material/Subject";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 function Assignment() {
 
-  const { currentUser, assignments } = useContext(UserContext); // Access user from UserContext
+  const { currentUser, assignments, getSubjectNameById  } = useContext(UserContext); // Access user from UserContext
 
   console.log(assignments);
   if (!currentUser) {
@@ -14,23 +16,61 @@ function Assignment() {
   }
 
   return (
+    // <Container>
+    //   <h1 className="my-4">Assignments</h1>
+    //   <Row>
+    //     {assignments.map(assignment => (
+    //       <Col key={assignment.assignmentid} md={4} className="mb-4">
+    //         <Card>
+    //           <Card.Body>
+    //             <Card.Title>{assignment.assignmentTitle}</Card.Title>
+    //             <Card.Text>{assignment.assignmentDescription}</Card.Text>
+    //             <Card.Text>Due Date: {new Date(assignment.duedate).toLocaleDateString()}</Card.Text>
+    //           </Card.Body>
+    //         </Card>
+    //       </Col>
+    //     ))}
+    //   </Row>
+    // </Container>
+
+
+  
     <Container>
-      <h1 className="my-4">Assignments</h1>
+      <h1 className="my-3">Assignments</h1>
+
       <Row>
         {assignments.map(assignment => (
-          <Col key={assignment.assignmentid} md={4} className="mb-4">
+          <Col key={assignment.assignmentid} md={3} className="mb-3">
             <Card>
               <Card.Body>
                 <Card.Title>{assignment.assignmentTitle}</Card.Title>
-                <Card.Text>{assignment.assignmentDescription}</Card.Text>
-                <Card.Text>Due Date: {new Date(assignment.duedate).toLocaleDateString()}</Card.Text>
+
+                <Card.Text>Slot {assignment.slotid}</Card.Text>
+
+                <Card.Title>   <SubjectIcon style={{ marginRight: "8px" }} /> {getSubjectNameById(assignment.subjectid)}</Card.Title>
+                
+             
+
+                <Card.Text className="d-flex align-items-center">
+                  <ClassIcon style={{ marginRight: "8px" }} />
+                  <div className="">SE1829-NJ</div>
+                </Card.Text>
+               
+                <Card.Text> <AccessTimeIcon style={{ marginRight: "8px" }} />  Due Date: {new Date(assignment.duedate).toLocaleDateString()}</Card.Text>
+            
+                <Link to={`/subject/${assignment.subjectid}/slot/${assignment.slotid}/question/${assignment.questionid}`} className="btn btn-primary">
+                  View assignment
+                </Link>
+
               </Card.Body>
             </Card>
           </Col>
         ))}
       </Row>
     </Container>
-  )
+  );
+
+  
 }
 
 export default Assignment;
